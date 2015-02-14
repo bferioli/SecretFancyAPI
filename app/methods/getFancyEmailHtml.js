aff9489f31634e430d03796ae160ac11211793ca
Ubuntu <ubuntu@ip-10-123-159-99.ec2.internal>
2015-02-14 21:11:07 +0000

    Add email template files

---
+++ app/methods/getFancyEmailHtml.js
@@ -0,0 +1,33 @@
+module.exports = function(fancy) {
+	var html =  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+		html += '<html xmlns="http://www.w3.org/1999/xhtml">';
+		html += ' <head>';
+		html += '  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+		html += '  <title>Demystifying Email Design</title>';
+		html += '  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+		html += '</head>';
+		html += '<body style="margin: 0; padding: 0;">';
+		html += '<table align="center" border="0" cellpadding="0" cellspacing="0" width="600">';
+		html += ' <tr>';
+		html += '  <td style="padding: 40px 0 20px 0; text-align: center;">';
+		html += '   <img src="http://secretfancy.com/images/logo.jpg" alt="SecretFancy" width="494" height="95" />';
+		html += '  </td>';
+		html += ' </tr>';
+		html += ' <tr>';
+		html += '  <td style="font-family: Helvetica, Arial, sans-serif; font-size: 16px; padding: 20px;">';
+		html += ' 	<p style="margin-bottom: 20px; ">';+fancy.first_name+',</p>';
+		html += ' 	<p style="margin-bottom: 50px; ">A secret admirer sent you a message on SecretFancy.</p>';
+		html += '	<a href="http://secretfancy.com/fancy/'+fancy._id+'" style="border-radius: 5px; background: #2591DA; color: #ffffff; padding: 7px 12px 7px 12px; text-decoration: none;"><font color="#ffffff">Read your message now</font></a>';
+		html += '  </td>';
+		html += ' </tr>';
+		html += ' <tr>';
+		html += '  <td style="color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 12px; padding: 100px 20px 20px 20px; text-align: center;">';
+		html += '   &copy; Copyright 2015 SecretFancy - All Rights Reserved';
+		html += '  </td>';
+		html += ' </tr>';
+		html += '</table>';
+		html += '</body>';
+		html += '</html>';
+
+	return html;
+};
